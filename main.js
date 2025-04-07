9bf4f3edcd8b0e44b63dd040a49867094ec4b65d
Jesse Wunderlich <jesse@wunderlich.com>
2025-04-06 22:34:06 -0500

    Fixed LinkedIn links

---
+++ main.js
@@ -0,0 +1,9 @@
+let userInput = 'rock';
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    return userInput;
+  } else {
+    console.log('Error. Please select either rock, paper, or scissors.');
+  }
+}
