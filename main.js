b6ce3f6574026e7d950885337710c8b1851b7d62
Jesse Wunderlich <jesse@wunderlich.com>
2025-07-22 17:05:57 +0300

    Remove main.js and .DS_Store files

--- main.js
+++
@@ -1,9 +0,0 @@
-let userInput = 'rock';
-const getUserChoice = userInput => {
-  userInput = userInput.toLowerCase();
-  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
-    return userInput;
-  } else {
-    console.log('Error. Please select either rock, paper, or scissors.');
-  }
-}
